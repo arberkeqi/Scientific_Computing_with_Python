0186dbe22d36fea7f6545f8657f8d6425aab3441
Arber Keqi <arberkeqi@gmail.com>
2022-07-23 14:06:13 -0700

    Commited

--- chapter_12/12_socket1.py
+++ chapter_12/12_socket1.py
@@ -2,7 +2,9 @@ import socket
 # socket still not connected; here some kind of file handler
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(("data.pr4e.org", 80))
-cmd = "GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n".encode()    # .encode() -> prepare the data to go across the internet
+cmd = "GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n".encode()    
+# .encode() -> prepare the data to go across the internet
+# -> takes a unicode string and turns it into a UTF-8 bites to transport it
 mysock.send(cmd)
 
 while True:
@@ -10,4 +12,5 @@ while True:
     if len(data) < 1:
         break
     print(data.decode(), end = '')
+    # .decode() -> takes a UTF-8 bite and turns it into a unicode string 
 mysock.close()
