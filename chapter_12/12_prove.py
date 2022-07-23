cc5cd659402189e70e2a41264c8bd864406d8b1c
Arber Keqi <arberkeqi@gmail.com>
2022-07-23 14:36:52 -0700

    Delete chapter_12 directory

--- chapter_12/12_prove.py
+++
@@ -1,7 +0,0 @@
-import socket
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a socket
-# (goes across the int, series of char come one after another)
-mysock.connect(("data.pr4e.org", 80))   # ("Host", Port)
-
-# http://www.dr-chuck.com/page1.html
-# protocol      host        document
