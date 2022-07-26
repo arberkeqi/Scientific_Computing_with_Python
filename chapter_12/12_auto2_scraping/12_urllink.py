b06575a5af2570d5950786bbf980be48d65bf245
Arber Keqi <arberkeqi@gmail.com>
2022-07-26 15:37:20 -0700

    Finally done

--- chapter_12/12_auto2_scraping/12_urllink.py
+++ chapter_12/12_auto2_scraping/12_urllink.py
@@ -1,7 +1,5 @@
-# To run this, download the BeautifulSoup zip file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
-
+# http://py4e-data.dr-chuck.net/comments_42.html
+import urllib.request, urllib.parse, urllib.error
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
@@ -11,15 +9,15 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urlopen(url, context=ctx).read()
+url = input('Enter: ')
+html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
-tags = soup('a')
+print()
+tags = soup('span')
+new_num = list()
 for tag in tags:
-    # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
+    contents = int(tag.contents[0])
+    new_num.append(contents)
+print("Sum:", sum(new_num))
