4e9d83863f375db1beeed5640a717dc261cea5ea
Arber Keqi <arberkeqi@gmail.com>
2022-07-30 13:08:02 -0700

    Commited

--- chapter_13/13_1auto_XML.py
+++ chapter_13/13_1auto_XML.py
@@ -2,42 +2,50 @@ import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
 
-api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
-
-if api_key is False:
-    api_key = 42
-    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
-else :
-    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
-
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    address = input('Enter location: ')
-    if len(address) < 1: break
+url = input("Enter link address: ")
+print("Retrieving", url)
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
-    url = serviceurl + urllib.parse.urlencode(parms)
-    print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
+uh = urllib.request.urlopen(url, context=ctx)
+data = uh.read()        # data here place the role of input down here
+print("Retrieved", len(data), "characters")
+# print(data.decode())
 
-    data = uh.read()
-    print('Retrieved', len(data), 'characters')
-    print(data.decode())
-    tree = ET.fromstring(data)
+tree = ET.fromstring(data)
+# need to create the path
+results = tree.findall("comments/comment")
+newNum = list()
 
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
+for num in results:
+    cnum = int(num.find("count").text)
+    newNum.append(cnum)
+print("Sum:", sum(newNum))
 
-    print('lat', lat, 'lng', lng)
-    print(location)
+
+# import xml.etree.ElementTree as ET
+# input = '''<stuff>
+#     <users>
+#         <user x="2">
+#             <id>001</id>
+#             <name>Chuck</name>
+#         </user>
+#         <user x="7">
+#             <id>009</id>
+#             <name>Brent</name>
+#         </user>
+#     </users>
+# </stuff>'''
+
+# stuff = ET.fromstring(input)
+# lst = stuff.findall('users/user')
+# print('User count:', len(lst))
+# for item in lst:
+#     id = int(item.find('id').text)
+#     print('Name', item.find('name').text)
+#     print('Id', id)
+#     print('Attribute', item.get("x"))
+#     print("ID type: ", type(id))
