7c720bf06607117bef70118f29fa801d6d945501
Arber Keqi <arberkeqi@gmail.com>
2022-08-05 17:57:22 -0700

    Commited

--- chapter_15/15_3auto_tracks/tracks.py
+++ chapter_15/15_3auto_tracks/tracks.py
@@ -22,12 +22,12 @@ cur.executescript('''
     CREATE TABLE Album (
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
         artist_id INTEGER,
-        title TEXT
+        title TEXT UNIQUE
     );
 
     CREATE TABLE Track (
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-        title UNIQUE,
+        title TEXT UNIQUE,
         album_id INTEGER,
         genre_id  INTEGER,
         len INTEGER, rating INTEGER, count INTEGER
